bdc7d0c6643167bee3ae2edc34f8a39a4acd378a
WebDux <68965859+vakh0@users.noreply.github.com>
2022-10-29 17:44:19 +0400

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
+# Coded by WebDux
 
-#Coded by - WebDux (vakh0) -
-
-a=$(echo "|-------[ TEMP ]------")
+a=$(echo "|------[ TEMP ]------|")
 b=$(sensors | grep 'CPU:\|fan1:')
-c=$(echo " |---------------------")
+c=$(echo " |--------------------|")
 
 for intro in $b
 do
@@ -13,12 +12,12 @@ done
 
 tempcels=${myarray[4]}
 strtemp="${tempcels//[!0-9]/}"
-limit=65
+limit=60
 
 if [[ $strtemp < $limit ]]
 	then
 		echo -e "\e[33m" $a '\e[0m'
-		echo "  FAN:" ${myarray[1]} ${myarray[2]}
+		echo -e "  \033[93mFAN:" ${myarray[1]} ${myarray[2]}
     	echo -e "" "\033[34m" ${myarray[3]} ${myarray[4]}
     	echo -e "\e[33m" $c
 	else if [[ $strtemp > $limit ]]
